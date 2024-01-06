31a0b0d3e22d6fbc7fd75590fd76925d0896b7ab
Marouane <marwan.nejmeddine@gmail.com>
2024-01-06 16:25:46 +0100

    initt

---
+++ constants/index.ts
@@ -0,0 +1,80 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Tailored Itineraries',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'offering personalized and customized travel plans to cater to the unique preferences and interests of each client. Tailored itineraries ensure a memorable and satisfying travel experience.',
+    },
+    {
+      title: 'Set an adventure schedule',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "Secure your travel plans well in advance by booking well ahead of your intended departure date. This ensures that you have ample time to prepare and organize your trip, allowing for a stress-free and well-thought-out travel experience.",
+    },
+    {
+      title: 'Local Expertise',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Providing in-depth knowledge and local expertise about various destinations with well-informed staff who can offer insights into the best attractions, hidden gems, and cultural nuances of each location, enhancing the overall travel experience.',
+    },
+    {
+      title: 'VIP Concierge Services',
+      icon: '/vip.svg',
+      variant: 'orange',
+      description:
+        'Offer a premium tier of services for clients seeking an elevated travel experience, including personalized itineraries, exclusive access to events, private tours, and dedicated concierge assistance throughout the journey.',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
